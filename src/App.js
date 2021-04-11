31a77d658963cd9092dcaace7879da0d73760b33
dsiddapp <dsiddapp@in.ibm.com>
2021-04-11 17:35:20 +0530

    collection component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component' ;
+import ShopPage from './pages/shop/shop.component' ;
 import {Switch, Route} from 'react-router-dom';
 
-const HatsPage = (props) => (
-   <div>
-    <h1>Hats page</h1>
-    {console.log(props)}
-  </div>
-  );
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/shop/hats' component={HatsPage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
       </Switch>
       
     </div>
